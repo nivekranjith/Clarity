591769a29cc3011972f118e11a8004541b4937db
sl1msn1per <sl1msn1per@live.com>
2016-09-18 12:51:02 +0000

    Changed defs. in matrix.cpp and implemented edge_extrapolation_pixel().

--- Code/matrix.cpp
+++ Code/matrix.cpp
@@ -4,9 +4,49 @@
 Matrix(int** matrix_in, int width, int height) {}
 ~Matrix() {}
 
-static void kernel(Matrix* matrix, RGB* source, int x, int y) {}
-static RGB* convolution(Matrix* matrix, RGB* source) {}
-static void edge_extrapolate_source(RGB* source) {}
-static RGBAPixel* edge_extrapolate_pixel(RGB* source, int x, int y) {}
+static void Matrix::kernel(Matrix* matrix, BMP* source, int x, int y) {}
+static RGB* Matrix::convolution(Matrix* matrix, BMP* source) {}
+static void Matrix::edge_extrapolate_source(BMP* source) {}
+static RGBAPixel* Matrix::edge_extrapolate_pixel(BMP* source, int x, int y) {
+  if ((x>=0)&&(x<source->tellWidth())&&(y>=0)&&(y<source->tellHeight())) {
+    return source->(x,y);
+  }
+  else {
+    //On the left
+    if (x<0) {
+      //Top-left.
+      if (y<0) {
+	return source->(0,0);
+      }
+      //Bottom-left
+      if (y>source->tellHeight()) {
+	return source->(0,source->tellHeight()-1);
+      }
+      //Left side somewhere
+      return source->(0,y);
+    }
+    //On the right
+    if (x>source->tellWidth()) {
+      //Top-right
+      if (y<0) {
+	return source(source->tellWidth()-1,0);
+      }
+      //Bottom-right
+      if (y>source->tellHeight()) {
+	return source(source->tellWidth()-1,source->tellHeight()-1);
+      }
+      //Right side somewhere
+      return source(tellWidth()-1,y)
+    }
+    //On the top
+    if (y<0) {
+      //Can only be on the top somewhere
+      return source->(x,0);
+    }
+    //Can only be on the bottom somewhere
+    return source->(x,source->tellHeight()-1);
+  }
+  return nullptr;
+}
 
 #endif
