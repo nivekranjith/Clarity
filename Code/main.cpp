466ee8298cf23791f706eb020749c39f70249ffc
Jadon Jesse <jadon_jesse@yahoo.com>
2016-10-01 16:42:06 +0200

    92.3 percent

--- Code/main.cpp
+++ Code/main.cpp
@@ -10,40 +10,60 @@ using namespace std;
 int main( int argc, char* argv[] ) 
 { 
 	double start,end;
+	double time;
+	int loop=10;
 	//Do Parallel
 	if(*argv[3]=='p' || *argv[3]=='P') {
 
 		BMP* Background = new BMP(); 
+		BMP* Output;
 		Background->ReadFromFile(argv[1]); 
 
 		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
 
-		start = omp_get_wtime();
-		BMP* Output = Matrix::convolution_parallel(mat,Background,4);
-		end = omp_get_wtime();
+		for(int i=0;i<loop;i++)
+		{
+			start = omp_get_wtime();
+			Output = Matrix::convolution_parallel(mat,Background,4);
+			end = omp_get_wtime();
+
+			time +=(end-start);
+			//cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
+		}
+
+		
 
 		delete mat;
 		  
 		Output->WriteToFile(argv[2]);
-		cout<<"Exection Time (Parallel) : "<<(end-start)<<endl;
+		cout<<"Exection Time (Parallel) : "<<(time/10)<<endl;
 
 	}
 	//Do Serial
     else if(*argv[3]=='s' || *argv[3]=='S'){
 
 		BMP* Background = new BMP(); 
+		BMP* Output;
 		Background->ReadFromFile(argv[1]); 
 		
 		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
 
-		start = omp_get_wtime();
-		BMP* Output = Matrix::convolution(mat,Background);
-		end = omp_get_wtime();
+		for(int i=0;i<loop;i++)
+		{
+			start = omp_get_wtime();
+			Output = Matrix::convolution(mat,Background);
+			end = omp_get_wtime();
+
+			time+=(end-start);
+			//cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
+		}
+
+
 
 		delete mat;
 		  
 		Output->WriteToFile(argv[2]);
-		cout<<"Exection Time (Serial) : "<<(end-start)<<endl;
+		cout<<"Exection Time (Serial) : "<<(time/loop)<<endl;
 
 	}
 
