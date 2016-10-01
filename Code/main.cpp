a28a6731c350cdd28ce3d589e99419cc15b3a18a
Jadon Jesse <jadon_jesse@yahoo.com>
2016-10-01 14:58:50 +0200

    92 percent

--- Code/main.cpp
+++ Code/main.cpp
@@ -2,19 +2,51 @@
 #include "EasyBMP/EasyBMP.h"
 #include "matrix.h"
 #include "kernels.h"
+#include "omp.h"
 #include <string.h>
 using namespace std;
 
 //Note that BMP's apparently automatically delete.
 int main( int argc, char* argv[] ) 
 { 
-  BMP* Background = new BMP(); 
-  Background->ReadFromFile(argv[1]); 
+	double start,end;
+	//Do Parallel
+	if(*argv[3]=='p' || *argv[3]=='P') {
 
-  Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
-  BMP* Output = Matrix::convolution_parallel(mat,Background,4);
-  delete mat;
-    
-  Output->WriteToFile(argv[2]);
+		BMP* Background = new BMP(); 
+		Background->ReadFromFile(argv[1]); 
+
+		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
+
+		start = omp_get_wtime();
+		BMP* Output = Matrix::convolution_parallel(mat,Background,4);
+		end = omp_get_wtime();
+
+		delete mat;
+		  
+		Output->WriteToFile(argv[2]);
+		cout<<"Exection Time (Parallel) : "<<(end-start)<<endl;
+
+	}
+	//Do Serial
+    else if(*argv[3]=='s' || *argv[3]=='S'){
+
+		BMP* Background = new BMP(); 
+		Background->ReadFromFile(argv[1]); 
+		
+		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
+
+		start = omp_get_wtime();
+		BMP* Output = Matrix::convolution(mat,Background);
+		end = omp_get_wtime();
+
+		delete mat;
+		  
+		Output->WriteToFile(argv[2]);
+		cout<<"Exection Time (Serial) : "<<(end-start)<<endl;
+
+	}
+
+
 return 0; 
 } 
