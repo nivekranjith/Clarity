22b5c22fa977a161a36433c2cf48ec63d433ebd3
Jadon Jesse <jadon_jesse@yahoo.com>
2016-10-01 19:29:05 +0200

    95 percent

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,3 +1,4 @@
+
 #include "EasyBMP/EasyBMP.h"
 #include "matrix.h"
 #include "kernels.h"
@@ -11,40 +12,44 @@ int main( int argc, char* argv[] )
 	double start,end;
 	double time;
 	int loop=10;
-
-
+	//Do Parallel
 	if(*argv[3]=='p' || *argv[3]=='P') {
 
-	  BMP* Background = new BMP(); 
-	  BMP* Output;
-	  Background->ReadFromFile(argv[1]); 
+		BMP* Background = new BMP(); 
+		BMP* Output;
+		Background->ReadFromFile(argv[1]); 
 
-	  Matrix* mat = Kernels::genMatrix(3,"box_blur");
+		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
 
-	  for(int i=0;i<loop;i++){
-	  	start = omp_get_wtime();
-	  	Output = Matrix::convolution_parallel(mat,Background,4);
-	  	end = omp_get_wtime();
+		for(int i=0;i<loop;i++)
+		{
+			start = omp_get_wtime();
+			Output = Matrix::convolution_parallel(mat,Background,4);
+			end = omp_get_wtime();
 
-	  	time+=(end-start);
-	  }
+			time +=(end-start);
+			//cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
+		}
 
-	  delete mat;
-	    
-	  Output->WriteToFile(argv[2]);
-	  cout<<"Exection Time (Parallel) : "<<(time/loop)<<endl;
+		
+
+		delete mat;
+		  
+		Output->WriteToFile(argv[2]);
+		cout<<"Exection Time (Parallel) : "<<(time/10)<<endl;
 
 	}
-
+	//Do Serial
     else if(*argv[3]=='s' || *argv[3]=='S'){
 
 		BMP* Background = new BMP(); 
 		BMP* Output;
 		Background->ReadFromFile(argv[1]); 
 		
-		Matrix* mat = Kernels::genMatrix(3,"box_blur");
+		Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
 
-		for(int i=0;i<loop;i++){
+		for(int i=0;i<loop;i++)
+		{
 			start = omp_get_wtime();
 			Output = Matrix::convolution(mat,Background);
 			end = omp_get_wtime();
@@ -59,9 +64,8 @@ int main( int argc, char* argv[] )
 		  
 		Output->WriteToFile(argv[2]);
 		cout<<"Exection Time (Serial) : "<<(time/loop)<<endl;
+
 	}
-
-
 
 return 0; 
 } 
