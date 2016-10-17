ff4cba7793355e7b5045695f1ef49595ef491e72
Liam Pulles <855442@students.wits.ac.za>
2016-10-17 14:12:30 +0200

    Working on cleaning code up

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,22 +8,26 @@
 #include <string.h>
 using namespace std;
 
-//Note that BMP* deletes itself at the end of scope.
-void images_test(int argc, char* argv[]) {
+//Note: BMP* deletes itself at the end of scope.
+
+int images_test(int argc, char* argv[]) {
   double start,end;
   double time;
   //Loop 10 times for accuracy
   int loop=10; 
+
+  BMP* Background = new BMP(); 
+  BMP* Output;
+  Background->ReadFromFile(argv[1]); 
+	
+  //Use a helper function to create a matrix.
+  Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
+
   //Do Parallel
   if(*argv[3]=='p' || *argv[3]=='P') {
-    BMP* Background = new BMP(); 
-    BMP* Output;
-    Background->ReadFromFile(argv[1]); 
-
-    Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
-
     for(int i=0;i<loop;i++)
     {
+      //We only measure the time taken to do the convolution of the whole image.
       start = omp_get_wtime();
       Output = Matrix::convolution_parallel(mat,Background,4);
       end = omp_get_wtime();
@@ -31,53 +35,46 @@ void images_test(int argc, char* argv[]) {
       time +=(end-start);
       cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
     }
-
-    delete mat;
-		  
-    Output->WriteToFile(argv[2]);
     cout<<"Execution Time (Parallel) : "<<(time/loop)<<endl;
 
   }
+
   //Do Serial
   else if(*argv[3]=='s' || *argv[3]=='S'){
 
-    BMP* Background = new BMP(); 
-    BMP* Output;
-    Background->ReadFromFile(argv[1]); 
-		
-    Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
-
     for(int i=0;i<loop;i++)
-      {
-	start = omp_get_wtime();
-	Output = Matrix::convolution(mat,Background);
-	end = omp_get_wtime();
+    {
+      start = omp_get_wtime();
+      Output = Matrix::convolution(mat,Background);
+      end = omp_get_wtime();
 
-	time+=(end-start);
-	cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
-      }
-
-    delete mat;
-		  
-    Output->WriteToFile(argv[2]);
-    cout<<"Execution Time (Serial) : "<<(time/loop)<<endl;
+      time+=(end-start);
+      cout<<i <<" : "<<time <<"(+"<<end-start<<")"<<endl;
+    }
+    cout<<"Execution Time (Serial): "<<(time/loop)<<endl;
   }
+
+  delete mat;
+  //No need to save image.
+  return 0;
 }
 
-void thread_test(int argc, char* argv[]) {
+int thread_test(int argc, char* argv[]) {
   double start,end;
   double time;
-  //Loop 10 times for accuracy
+
+  //Loop 10 times for accuracy (per thread count)
   int loop=10;
   int threads_max=24;
-  //Do Parallel
+
   BMP* Background = new BMP(); 
   BMP* Output;
+
   Background->ReadFromFile(argv[1]); 
 
   Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
 
-  //"0" threads is serial implementation
+  //"0" is serial implementation
   for (int j=0; j<loop; j++) {
     start = omp_get_wtime();
     Output = Matrix::convolution(mat,Background);
@@ -87,7 +84,7 @@ void thread_test(int argc, char* argv[]) {
     cout << 0 << ' ' << time << endl;
   }
 
-  //Parallel implementation
+  //Parallel implementation (1-...)
   for(int i=1;i<threads_max;i++)
   {
     for (int j=0; j<loop; j++) {
@@ -96,20 +93,83 @@ void thread_test(int argc, char* argv[]) {
       end = omp_get_wtime();
 
       time = (end-start);
-          cout << i << ' ' << time << endl;
+      cout << i << ' ' << time << endl;
     }
   }
 
   delete mat;
-		  
-  //Output->WriteToFile(argv[2]);
-  //cout<<"Execution Time (Parallel) : "<<(time/loop)<<endl;
+  return 0;
 }
 
-//Arguments to the executable are described in section 3 of the report.
+int simple( int argc, char* argv[] )
+{
+  BMP* Background = new BMP(); 
+  BMP* Output;
+
+  Background->ReadFromFile(argv[1]); 
+
+  Matrix* mat = Kernels::genMatrix(5,"gaussian_blur");
+
+  double start,end;
+  double time;
+
+  //Do Parallel
+  if(*argv[3]=='p' || *argv[3]=='P') {
+    start = omp_get_wtime();
+    Output = Matrix::convolution_parallel(mat,Background,4);
+    end = omp_get_wtime();
+
+    time +=(end-start);
+    cout<<"Execution Time (Parallel) : "<<time<<endl;
+  }
+  //Do Serial
+  else if(*argv[3]=='s' || *argv[3]=='S'){
+    start = omp_get_wtime();
+    Output = Matrix::convolution(mat,Background);
+    end = omp_get_wtime();
+
+    time+=(end-start);
+    cout<<"Execution Time (Serial) : "<<time<<endl;
+  }
+  delete mat;	
+	  
+  //Save the image
+  Output->WriteToFile(argv[2]);
+
+  return 0;
+}
+
+//Arguments to the executable are as follows:
+// 1: input filename
+// 2: output filename
+// 3: parallel or serial
+// 4: Choice between 2 tests or straight run.
 int main( int argc, char* argv[] ) 
-{ 	
-  //images_test(argc,argv);
-  thread_test(argc,argv);
-  return 0; 
+{ 
+  string argv3(argv[3]);
+  if (argc==4) {
+    if (argv3=="thread") {
+      return thread_test(argc,argv);
+    }
+    else {
+      cout<<"To use 3 arguments only the 3rd argument must be \"thread\" (without quotes).\n3rd argument is \""<<argv3<<"\"\n";
+    }
+  }
+  else {
+    if (argc != 5) {
+      printf("Number of arguments must be 3 or 4.\n");
+      return 1;
+    }
+    string argv4(argv[4]);
+    if(argv4=="simple") {
+      return simple(argc,argv);
+    }
+    else if (argv4=="images") {
+      return images_test(argc,argv);
+    }
+    else {
+      printf("Argument \"%s\" (without quotes) not recognized.\n",argv[4]);
+    }
+  }
+  return 1; 
 } 
